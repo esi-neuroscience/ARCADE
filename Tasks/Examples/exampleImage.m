500b14bea61b2b22e4f7da02498eb2832d2cd4f7
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2016-09-19 15:37:47 +0200

    Added example for images

---
+++ Tasks/Examples/exampleImage.m
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+% timing 
+trl.stimDuration  = randi([1900,2100],1); 
+trl.itiDuration   = 500;
+
+
+%% DEFINE AND CREATE STIMULI
+
+% image 1
+img1 =  [];
+img1.imageFile = 'E:\ARCADE\Tasks\Examples\exampleImage1.bmp';
+img1.position = [200 350];
+img1.alpha = 128;
+%img1.angle = 30;
+IMG1 = createNewStimulus('Image', img1);
+
+% image 2
+img2 =  [];
+img2.imageFile = 'E:\ARCADE\Tasks\Examples\exampleImage2.bmp';
+img2.position = [300 350];
+img2.alpha = 100;
+
+IMG2 = createNewStimulus('Image', img2);
+
+%% DEFINE AND CREATE TRIAL STATES
+
+%# STIMULUS ON
+sStimOn = [];
+sStimOn.name      = 'stimOn';
+sStimOn.duration  = trl.stimDuration;
+sStimOn.nextState = 'cleanUp';
+sStimOn.onEntry   = {...    
+    {@() IMG1.visible(true)},...
+    {@() IMG2.visible(true)},...
+    }    
+sStimOn.onExit    = {...
+    {@() IMG1.visible(false)}, ...
+    {@() IMG2.visible(false)}};
+
+
+%# END OF TRIAL CLEANUP 
+sCleanUp = [];
+sCleanUp.name      = 'cleanUp';
+sCleanUp.duration  = 0;
+sCleanUp.nextState = 'iti';
+sCleanUp.onEntry = {...    
+    {@() IMG1.visible(false)},...
+    {@() IMG2.visible(false)},...
+    };
+sCleanUp.onExit  = {...
+    {@() IMG1.delete}, ...
+    {@() IMG2.delete}, ...
+    };
+
+%# ITI
+sITI = [];
+sITI.name      = 'iti';
+sITI.duration  = trl.itiDuration;
+sITI.nextState = 'final';
+
+
+initialState = 'stimOn';
+createTrial(initialState,...
+    sStimOn,...
+    sCleanUp, ...
+    sITI);
+
+
--- arcade/start_arcade.bat
+++ arcade/start_arcade.bat
@@ -3,5 +3,5 @@ SETLOCAL
 :: 32-bit version of matlab 2014a
 PATH "C:\Program Files (x86)\MATLAB\R2014a\bin\"
 SET DISPLAYDIR="%~dp0"\ProcessLaunch\
-SET RUNCMD=matlab -nodesktop -r run('%DISPLAYDIR%runCore.m');
+SET RUNCMD=matlab -nodesktop -r "dbstop if error ; run('%DISPLAYDIR%runCore.m')";
 %RUNCMD%
