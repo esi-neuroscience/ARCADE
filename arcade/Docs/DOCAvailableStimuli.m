01507a190228f26ed9e4f2edc92f59b3a0a2664e
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2016-11-23 10:00:19 +0100

    Added other image formats for testing

--- arcade/Docs/DOCAvailableStimuli.m
+++ arcade/Docs/DOCAvailableStimuli.m
@@ -1,10 +1,12 @@
 %% Available stimuli for ARCADE
+% Stimuli are created in the task script using the |createNewStimulus|
+% function. Possible stimuli are described here.
 % 
 %% Images
 %%
 %   imgDefinition =  [];
 %   imgDefinition.imageFile = 'C:\ARCADE\Tasks\Examples\imageFile.bmp';
-%   imgDefinition.position = [200 350]; % pixel
+%   imgDefinition.position = [200 350]; % pixel from screen center
 %   imgDefinition.alpha = 128; % 0-255
 %   imgDefinition.angle = 30; % degree
 %   IMG = createNewStimulus('Image', imgDefinition);
@@ -15,6 +17,20 @@
 % transparent and 255 fully opaque. Transparency in PNGs is also supported.
 
 %% Masked grating
+def =  [];
+def.position       = [0,0];             % [x,y] position (px)
+def.color0         = [ 0, 255, 0,150];  % [r,g,b,w] [0...255] color when sinewave is 0
+def.color1         = [255,0,0,150];     % [r,g,b,w] [0...255] color when sinewave is 1
+def.pixelsPerCycle = 100;               % [>=1] pixels per cycle of grating
+def.direction      = 90;                 % direction of movement (orthogonal to orientation)
+def.pixelsPerFrame = 3;                 % each frame shift by N pixels
+def.phaseShift     = 0;                 % grating degrees
+def.smoothing      = 2;                 % sinewave == 2, values greater tend to squarewave ~10
+def.maskRadii      = [200 100];               % circle mask [x,y] elliptical
+def.maskRotation   = 0;                 % degrees
+
+%     gratingObject = createNewStimulus('MaskedGrating', def);
+
 
 %% Random dot patterns
 %%
