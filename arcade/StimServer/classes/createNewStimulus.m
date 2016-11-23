488da35a04d1c620c6971e7f94e9a1ace3d85d19
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2016-11-23 09:47:03 +0100

    Added documentation for createNewStimulus.m

--- arcade/StimServer/classes/createNewStimulus.m
+++ arcade/StimServer/classes/createNewStimulus.m
@@ -1,58 +1,54 @@
-
-
-
-
-%---------------------------------------------------%
-% function description
-%---------------------------------------------------%
-
-
-
-%------------------------------------------%
-% 17.4.2016 - Jarrod, wrote function 
-% 21.4.2016 - Jarrod, added MovingBar and StaticBar
-% 26.4.2016 - Jarrod, added Image and BitmapBrush
-
-
 function newStimObj = createNewStimulus(objectName, params)
-% wrapper for [HIGH LEVEL OBJECTS]
+% Create a new stimulus object
+%
+% INPUT
+% -----
+%   objectName : string indicating the type of the stimulus. Possible
+%                options are 'FilledDot', 'StaticBar', 'MovingBar',
+%                'MaskedGrating', 'RandomDots', 'Image'
+%       params : struct with stimulus parameters as fields. See 
+%                Docs/DOCAvailableStimuli or HLO classes for details.
+% 
+% OUTPUT
+% ------
+%   newStimObj : stimulus object that can be used to manipulate stimulus 
+%                in trial states, e.g. change visibility
 
-newStimObj = []; %#ok<NASGU> 
+newStimObj = []; %#ok<NASGU>
 
 switch objectName
     case 'FilledDot'
         % static solid filled dot
         newStimObj = HLOFilledDot;
     case 'StaticBar'
-        % static bar 
+        % static bar
         newStimObj = HLOStaticBar;
     case 'MovingBar'
-        % straight paths only 
+        % straight paths only
         newStimObj = HLOMovingBarLinearPath;
     case 'MaskedGrating'
-        % static/moving, circle/ellipse/square, 
+        % static/moving, circle/ellipse/square,
         % color/bw, sine/squarewave gratings
         newStimObj = HLOMaskedGrating;
     case 'RandomDots'
-        % moving/static random dots 
+        % moving/static random dots
         newStimObj = HLORandomDots;
     case 'Image'
-        % images 
+        % images
         newStimObj = HLOImage;
     case 'BitmapBrush'
-        % images that allow an opacity mask 
+        % images that allow an opacity mask
         newStimObj = HLOBitmapBrush;
     case 'GratingCutout'
         newStimObj = HLOGratingCutout;
-    otherwise 
+    otherwise
         error(['Unknown Stimulus Type: ', objectName]);
 end
 
-% if object was created, then create the stimuulus 
+% if object was created, then create the stimuulus
 if ~isempty(newStimObj)
     newStimObj.createStimulus(params);
 end
 
-    
 end
 
