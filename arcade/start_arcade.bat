5f351fef767b0c62a2fff33d1f2058513b0239ff
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2016-12-20 16:38:23 +0100

    added debug mode for startup script

--- arcade/start_arcade.bat
+++ arcade/start_arcade.bat
@@ -3,5 +3,11 @@ SETLOCAL
 :: 32-bit version of matlab 2014a
 PATH "C:\Program Files (x86)\MATLAB\R2014a\bin\"
 SET DISPLAYDIR="%~dp0"\ProcessLaunch\
-SET RUNCMD=matlab -nodesktop -r "dbstop if error ; run('%DISPLAYDIR%runCore.m')";
+
+IF "%~1"=="/DEBUG" (
+	SET RUNCMD=matlab -r "dbstop if error; run('%DISPLAYDIR%runCore.m')";
+) ELSE (
+	SET RUNCMD=matlab -nodesktop -r "run('%DISPLAYDIR%runCore.m')"
+)
+
 %RUNCMD%
