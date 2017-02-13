0802de55870bd2790dd3abec0d09997124dbd128
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-02-13 09:28:21 +0100

    bla

--- arcade/Tests/EyeServer/test_EyeServer.m
+++ arcade/Tests/EyeServer/test_EyeServer.m
@@ -1,26 +1,35 @@
+clear
+close all
+
 EyeServer = SGLEyeServer.launch();
 
-
 %% test fetching
-dt = 0.001;
-nMin = 60*24*5;
-nSamples = round(nMin*60/dt);
-plotEyePosition = true;
-
-tic
-
-
+nMin = 60*24*5; % 5 days
+% nMin = 1;
+plotEyePosition = false;
 
 if plotEyePosition
     figure('WindowStyle', 'normal')
     ax = axes('position', [0.07 0.07 0.9 0.9]);
-%     axis equal
     set(ax, 'NextPlot', 'replacechildren', 'XLim', [-0.5 0.5]*1680, 'YLim', [-0.5 0.5]*1050)
-end    
-
-for iSample = 1:nSamples
-    eye_pos = EyeServer.mFetchEyePosition();
-    plot(eye_pos(1), eye_pos(2), 'o')
-    pause(dt);
 end
-toc
+
+nSamples = 0;
+tic
+while toc<nMin*60
+    eye_pos = EyeServer.mFetchEyePosition();
+    if plotEyePosition
+        plot(eye_pos(1), eye_pos(2), 'o')
+        drawnow()
+    end
+    if mod(nSamples, 1000) == 0
+        fprintf('%g s, %u samples\n',toc, nSamples)
+    end
+    nSamples = nSamples+1;
+    java.lang.Thread.sleep(10)
+    
+    
+end
+duration = toc;
+fprintf('Sampled %u datapoints in %g min\n', nSamples, duration/60)
+nIdealSamples = nMin*60*100
