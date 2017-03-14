66e136d73778af483e388c8f99af698ebd7e13c1
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-03-14 17:06:09 +0100

    added test foe EyeServer pipes

---
+++ arcade/Tests/Pipes/test_EyePipes.m
@@ -0,0 +1,20 @@
+
+%% launch eye server
+
+launchCmd = 'matlab  -nosplash -r "CorePipe = SGLEyeServerCorePipe.launch;CorePipe.mOpenServer;" &';
+
+% launch process
+runtime = java.lang.Runtime.getRuntime();
+process = runtime.exec(launchCmd);
+
+%% SGLEyeServerCorePipe
+EyeCorePipe = SGLEyeServerCorePipe.launch();
+EyeCorePipe.mOpenClient();
+
+%%
+EyeCorePipe.mWriteCommandMessage('start_eye');
+%%
+while true
+    EyeCorePipe.mRequestEyeData()
+    pause(0.2)
+end
