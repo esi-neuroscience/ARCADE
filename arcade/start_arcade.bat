a091cc5f7f0b262d06214bede7bd0d5686c22ef8
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-04-18 15:55:23 +0200

    Fix: broken Windows path setting in start script

--- arcade/start_arcade.bat
+++ arcade/start_arcade.bat
@@ -1,13 +1,14 @@
 @Echo OFF
 SETLOCAL
 :: 64-bit version of matlab 2014a
-PATH "C:\Program Files\MATLAB\R2014a\bin\"
+SET PATH=%PATH%;"C:\Program Files\MATLAB\R2014a\bin\"
+echo PATH
 SET DISPLAYDIR="%~dp0"\ProcessLaunch\
 
 IF "%~1"=="/DEBUG" (
 	SET RUNCMD=matlab -r "dbstop if error; run('%DISPLAYDIR%runCore.m')";
 ) ELSE (
-	SET RUNCMD=matlab -nodesktop -r "run('%DISPLAYDIR%runCore.m')"
+	SET RUNCMD=matlab -nodesktop -nosplash -r "run('%DISPLAYDIR%runCore.m')"
 )
 
 %RUNCMD%
