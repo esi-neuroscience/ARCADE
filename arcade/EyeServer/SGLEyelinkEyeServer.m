2f64fc31c936a286b2c687e30a01b30b1c76f703
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-04-18 15:56:34 +0200

    improvements of EyelinkEyeServer

--- arcade/EyeServer/SGLEyelinkEyeServer.m
+++ arcade/EyeServer/SGLEyelinkEyeServer.m
@@ -1,22 +1,50 @@
 classdef SGLEyelinkEyeServer < ABSEyeServer
     
+    properties
+        filename = [];
+        usedEye = 0;
+    end
+    
+    
+    properties ( Access = private )
+        folder = [];
+        shortFilename = [];
+    end
+    
     methods (Static)
-        function this = launch
+        function this = launch(filename)
             persistent thisObj
             if isempty(thisObj) || ~isvalid(thisObj)
-                thisObj = SGLTestEyeServer;
+                if nargin == 0
+                    filename = [];
+                end
+                thisObj = SGLEyelinkEyeServer(filename);
+                
             end
             this = thisObj;
         end
     end
     
     methods ( Access = private )
-        function this = SGLEyelinkEyeServer()
-            this = this@ABSEyeServer;            
-            status = Eyelink('Initialize')
+        function this = SGLEyelinkEyeServer(filename)
+            this = this@ABSEyeServer;
+            this.filename = filename;
+            
+            
+            
+            err = Eyelink('Initialize');
+            assert(err == 0, 'Eyelink: Could not initialize link')
             Eyelink('Command', 'link_sample_data = LEFT,RIGHT,GAZE,AREA');
-            edfFile='demo.edf';
-            Eyelink('Openfile', edfFile);
+            if ~isempty(this.filename)
+                [this.folder,base,ext] = fileparts(filename);
+                if length(base) > 8
+                    warning('Eyelink: filename will be shortened on Eyelink PC to %s', base(1:8))
+                    base = base(1:8);
+                end
+                this.shortFilename = [base ext];
+                err = Eyelink('OpenFile', this.shortFilename);
+                assert(err == 0, 'Eyelink: Could not open %s', this.filename)
+            end
             
         end
     end % private methods
@@ -27,40 +55,51 @@ classdef SGLEyelinkEyeServer < ABSEyeServer
         
         function start(this)
             MISSING_DATA=-32768;
-            eye_used = 0;
-            stopEvent = IPCEvtServer('StopEyelink');
+            
+            stopEvent = IPCEvtServer('StopEyeServer');
             
             
-            err =  Eyelink('StartRecording'); % [,file_samples, file_events, link_samples, link_events] )
-            Eyelink('Message', 'SYNCTIME');
+            err =  Eyelink('StartRecording');
+            assert(err == 0, 'Eyelink: Could not start recording')
+            err = Eyelink('Message', 'SYNCTIME');
+            assert(err == 0, 'Eyelink: Could not reset time to zero')
             
             
-            while ~stopEvent.wasTriggered 
+            while ~stopEvent.wasTriggered && Eyelink('CurrentMode') == 4
                 if Eyelink('NewFloatSampleAvailable') > 0
                     % get the sample in the form of an event structure
                     evt = Eyelink('NewestFloatSample');
-                    x = evt.gx(eye_used+1); % +1 as we're accessing MATLAB array
-                    y = evt.gy(eye_used+1);
-                    if x~=MISSING_DATA && y~=MISSING_DATA && evt.pa(eye_used+1)>0
+                    x = evt.gx(this.usedEye+1); % +1 as we're accessing MATLAB array
+                    y = evt.gy(this.usedEye+1);
+                    if x~=MISSING_DATA && y~=MISSING_DATA && evt.pa(this.usedEye+1)>0
                         this.sharedMemory.pointer.Value = [x; y];
                     end
                 end
                 java.lang.Thread.sleep(1)
+                drawnow
             end
             this.stop()
         end
         
         
-        function this = stop(this)
+        function stop(this)
             Eyelink('StopRecording');
-            Eyelink('CloseFile');
-            status=Eyelink('ReceiveFile');
+            
+            if ~isempty(this.filename);
+                err = Eyelink('CloseFile');
+                assert(err == 0, 'Eyelink: Could not close data file')
+                fprintf('Receiving data file\n')
+                nBytes = Eyelink('ReceiveFile');
+                if ~isequal(fullfile(pwd, this.shortFilename), this.filename)                    
+                    movefile(this.shortFilename, this.filename);
+                end
+                fprintf('Stored %g MB in %s\n', nBytes/1024/1024, this.filename)
+            end
             
         end
         
         
         function delete(this)
-            this.stop()
             Eyelink('Shutdown');
         end
     end % public methods
