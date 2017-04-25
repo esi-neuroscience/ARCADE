d4bc21ce3e85d6ee21a9800eb91b3cc71a4f6c2b
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-04-25 15:17:40 +0200

    FIX: Windows PATH problems in start_arcade.bat

--- arcade/start_arcade.bat
+++ arcade/start_arcade.bat
@@ -1,7 +1,7 @@
 @Echo OFF
 SETLOCAL
 :: 64-bit version of matlab 2014a
-SET PATH="C:\Program Files\MATLAB\R2014a\bin\";%~dp0StimServer\;%~dp0External\EyelinkToolbox"
+SET PATH="C:\Program Files\MATLAB\R2014a\bin\";"C:\Program Files\MATLAB\R2014a\bin\win64";C:\Windows\system32;C:\Windows;%~dp0StimServer\;%~dp0External\EyelinkToolbox"
 SET ARCADEDIR="%~dp0"\ProcessLaunch\
 
 IF "%~1"=="/DEBUG" (
