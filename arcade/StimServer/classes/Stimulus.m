93150ae235e07526f805a5b48d409dded8c46d94
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-05-15 11:45:02 +0200

    NEW: added animation interface

--- arcade/StimServer/classes/Stimulus.m
+++ arcade/StimServer/classes/Stimulus.m
@@ -1,8 +1,10 @@
 classdef Stimulus < hgsetget % will be matlab.mixin.SetGet after 2014b
     
-    properties ( SetAccess = public, GetAccess = public )
-        position = [0 0];
+    properties ( SetAccess = public, GetAccess = public )        
         visible = false;
+        protected = false;
+        position = [0 0];
+        animation = [];
     end
     
     properties ( SetAccess = immutable, GetAccess = public, Hidden = false )
@@ -17,8 +19,7 @@ classdef Stimulus < hgsetget % will be matlab.mixin.SetGet after 2014b
         end
                 
         function set.visible(obj, visible)
-            StimServer.Command(obj.key, ...
-                int8([0 visible])); 
+            StimServer.Command(obj.key, [0 visible]); 
             obj.visible = visible;
         end
         
@@ -27,13 +28,30 @@ classdef Stimulus < hgsetget % will be matlab.mixin.SetGet after 2014b
             x = value(1);
             y = value(2);
             StimServer.Command(obj.key, ...
-                [uint8(3) typecast(single([x y]), 'uint8')]);
+                [3 typecast(single([x y]), 'uint8')]);
             obj.position = value;
-        end                
+        end      
+        
+        function set.protected(obj, value)
+           error('Not implemented yet') 
+        end
+        
+        
+        function set.animation(obj, animation)            
+            if ~isempty(animation)
+                StimServer.Command(animation.key, ...
+                    [0 1 typecast(uint16(obj.key), 'uint8')])              
+            elseif isempty(animation) && ~isempty(obj.animation)
+                StimServer.Command(obj.animation.key, ...
+                    [0 0 typecast(uint16(obj.key), 'uint8')])                
+            end
+            obj.animation = animation;
+            
+        end
         
         function delete(obj)
             if ~isequal(obj.key, 0)
-                StimServer.Command(obj.key, int8(0));
+                StimServer.Command(obj.key, 0);
             end
         end
     end
