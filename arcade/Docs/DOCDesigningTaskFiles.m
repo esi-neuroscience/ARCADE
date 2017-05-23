0e06b9a09664dfa5b76041adb89d39b40d764054
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-05-23 19:09:47 +0200

    Updated documentation

--- arcade/Docs/DOCDesigningTaskFiles.m
+++ arcade/Docs/DOCDesigningTaskFiles.m
@@ -9,21 +9,24 @@
 % # Definition of trial states
 
 %% Creating the stimuli
-% Each stimulus is first defined with several stimulus type-specific parameters
-% in a struct array and then "created" as an object used by M-ARCADE.
+% Each stimulus is first defined with several stimulus type-specific
+% parameters. The stimulus will then be turned on in one of the trial
+% states.
+% 
 %   
 %   % Code that creates stimuli
-%   targetDefintion = [];
-%   targetDefinition.position = [4 -1];
-%   targetDefinition.color = [0 255 0 128];
+%   target = Circle;
+%   target.position = [4 -1];
+%   target.color = [0 255 0 128];
 %   ...
-%   targObj = createNewStimulus('MaskedGrating', targetDefinition);
+% 
 %
 % Documentation of all possible stimuli can be found in <DOCAvailableStimuli.html>.
 
 %% Creating trial states
-
 %%
+% *Example*
+% 
 %   sAcquireFix = [];
 %   sAcquireFix.name      = 'acqFix';
 %   sAcquireFix.duration  = trl.acqFixTime;
@@ -33,9 +36,15 @@
 %       {@() eventmarker(trig.acqFixation)}};
 %   sAcquireFix.onRunNum.onEntry{1} = {... % only evaluate on the first entry
 %       {@() eventmarker(trig.FixSpotOn)},...
-%       {@() groupStimuli('begin')},...
-%       {@() fixObj.visible(true)},... 
+%       {@() groupStimuli('start')},...
+%       {@() set(fixObj, 'visible', true)},... 
 %       {@() backgroundColor([128,128,128])},...
 %       {@() groupStimuli('end')}};
 %   sAcquireFix.onRunNum.onEntry{2} = {... % on second entry change duration
 %       {@(me) set(me,'duration',trl.reAcqFixTime), 'self'}};
+%  ...
+%  initialState = 'acqFix';
+%  createTrial(initialState,...
+%      sAcquireFix, 
+%      ...
+%      );
