6e07e88f0aa43f457e5b2b3b92028dc38da99669
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-06-22 12:58:03 +0200

    CHG: Updated createTrial.m for new state system

--- arcade/Core/loner functions/createTrial.m
+++ arcade/Core/loner functions/createTrial.m
@@ -4,44 +4,32 @@ function createTrial(initialState, varargin)
 % INPUT 
 % -----
 %   initialState : name of first state as string
-%   state1 : definitions of state as struct array with fields
-%       .name    :  name of state as string
-%       .onEntry :  cell array of anonymous functions to be executed when
-%                   the state is started
-%       .onExit  :  cell array of anonymous functions to be executed when
-%                   the state ends
-%       .duration:  duration of state in ms if no eye tracking event is
-%                   triggered
-%       .nextState: name of next state to be run if duration expires and no
-%                   eye tracking event is triggered
-%       .trackEye:  definition of eye tracking behavior
-%   state2 : ...
-%   state3 : ...
+%   states
 % 
-% The state with nextStat='final' will be the last state of the trial.
+% The state with nextState='final' will be the last state of the trial.
 %
 %
 % Example
 % -------
 %   initialState = 'acqFix';
-%   sAcquireFix = [];
-%   sAcquireFix.name      = 'acqFix';
+%   sAcquireFix = State('acqFix')
 %   sAcquireFix.duration  = 5000;
 %   sAcquireFix.nextState = 'ignore';         
-%   sAcquireFix.trackEye  = eyesEnterFixation;  
 %   sAcquireFix.onEntry   = {...
 %       {@() eventmarker(1)}, ...
 %       {@() set(fixationPoint, 'visible', true)};
 %   ... 
 %   createTrial(initialState, sAcquireFix, ...)
 %   
-% See also <a
-% href="https://de.mathworks.com/help/matlab/matlab_prog/anonymous-functions.html">Anonymous Functions</a>, TRLState
+% See also State
 
 
 % add states to state arc
 stateArc = SGLStateArc.launch;
-stateArc.mAddState(varargin{:});
+stateArc.states = [varargin{:}];
+if ~ismember(initialState, stateArc.stateNames)
+    error('Initial state %s is not among defined states', initialState)
+end
 stateArc.initialState = initialState;
 
 end
