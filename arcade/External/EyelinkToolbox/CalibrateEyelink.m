834517bc1e93f4493c627063706fd59ae0d11cc6
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-07-05 16:30:40 +0200

    CHG: more or less working version of Eyelink calibrator

--- arcade/External/EyelinkToolbox/CalibrateEyelink.m
+++ arcade/External/EyelinkToolbox/CalibrateEyelink.m
@@ -8,16 +8,30 @@ classdef CalibrateEyelink < handle
         stim
     end
     
+    properties ( Constant, Access = private, Hidden=true )
+        IN_TARGET_MODE=8
+        KB_PRESS = 10; % Eyelink.h
+        specialKeysNames = {'return', 'space', 'pageup', 'pagedown', 'escape', ...
+            'leftarrow', 'rightarrow', 'uparrow', 'downarrow', ...
+            'backspace'}
+        specialKeysValues = {hex2dec('000D'), 32, hex2dec('4900'), hex2dec('5100'), hex2dec('001B'), ...
+            hex2dec('4B00'), hex2dec('4D00'), hex2dec('4800'), hex2dec('5000'), ...
+            hex2dec('008')};
+    end
+    
+    properties ( GetAccess=private, SetAccess=immutable )
+        stimServerProcess
+        stopEvent
+    end
     
     methods
-        function obj = CalibrateEyelink(eyelinkConfig)
-               if ~Eyelink('IsConnected')
-                assert(Eyelink('InitializeDummy') == 0, 'Eyelink: could not initialize')
+        function obj = CalibrateEyelink(eyelinkConfig, stim)
+            if ~Eyelink('IsConnected')
+                assert(Eyelink('Initialize') == 0, 'Eyelink: could not initialize')
             end
             
             if nargin == 0
                 eyelinkConfig = EyelinkConfig();
-                %                 eyelinkConfig.read_config_from_tracker();
             end
             
             if ischar(eyelinkConfig)
@@ -25,60 +39,128 @@ classdef CalibrateEyelink < handle
                 eyelinkConfig = cfg.eyelinkConfig;
             end
             obj.eyelinkConfig = eyelinkConfig;
-            %             obj.eyelinkConfig.send_config_to_tracker();
+            obj.eyelinkConfig.send_config_to_tracker();
             
             
             obj.fig = figure('Color', 'w', 'WindowStyle', 'normal', 'MenuBar', 'None');
-            set(obj.fig, 'WindowKeyPressFcn', @obj.onKeyPress)
+            set(obj.fig, 'WindowKeyPressFcn', @obj.onKeyPress, ...
+                'CloseRequestFcn', @obj.onClose)
+            
             obj.ax = axes('SortMethod','childorder', ...
                 'XLim', eyelinkConfig.screen_pixel_coords([1 3]), ...
                 'YLim', eyelinkConfig.screen_pixel_coords([2 4]) ...
                 );
             obj.dot = line('XData', 0,'YData', 0,...
-                'Marker','o','color','k', 'MarkerSize', 40);
+                'Marker','o','color','k', 'MarkerSize', 40);                             
             
-         
+            % start StimServer
+            cmd = fullfile(arcaderoot, 'arcade', 'StimServer', 'StimServer.exe');
+            obj.stimServerProcess = processManager('command', cmd);
             
-            for iCalTarget = 1:2:numel(obj.eyelinkConfig.calibration_targets)
-                pos = obj.eyelinkConfig.calibration_targets(iCalTarget:iCalTarget+1);
-                line('XData', pos(1),'YData', pos(2),...
-                    'Marker','o','color', [0.2 0.2 0.2], 'MarkerSize', 20);
+            
+            tWait = tic;
+            success = false;
+            while ~success && toc(tWait) < 5
+                try 
+                    StimServer.Connect();
+                    success = true;
+                catch
+                end
+                pause(0.5);
             end
             
-            %             obj.start();
-            %             stim = Grating;
+            if nargin < 2
+                stim1 = Gaussian;
+                stim1.color = [255 255 255]; stim1.sdx = 10; stim1.sdy = 10;
+                
+                stim2 = Gaussian;
+                stim2.color = [0 0 0]; stim2.sdx = 2; stim2.sdy = 2;
+                
+                obj.stim = [stim1 stim2];
+            elseif nargin == 2
+                obj.stim = stim;
+            end
+            
+            
+            obj.stopEvent = IPCEvent('StopEyelinkCalibration');
+            obj.stopEvent.CreateEvent;
+            obj.start()
+            
+            
+            if nargout == 0
+                clear obj
+            end
+            
             
         end
         
         function start(obj)
-            el.IN_IDLE_MODE=1;    			% off-line
-            el.IN_SETUP_MODE=2;   			% setup or cal/val/dcorr
-            el.IN_RECORD_MODE=4;    		% data flowing
-            el.IN_TARGET_MODE=8;
-            stop = 0;
+            
             if ~Eyelink('IsConnected')
-                assert(Eyelink('InitializeDummy') == 0, 'Eyelink: could not initialize')
+                assert(Eyelink('Initialize') == 0, 'Eyelink: could not initialize')
             end
             Eyelink('StartSetup');
+            Eyelink('SendKeyButton', double('c'), 0, obj.KB_PRESS)
             
-            
-            while ~stop %&& bitand(Eyelink('CurrentMode'), el.IN_TARGET_MODE)
+            while ~obj.stopEvent.wasTriggered %&& (Eyelink('CurrentMode') == 10)
+                
+                [~, statusStr] = Eyelink('ReadFromTracker', 'calibration_status');
+                status = strsplit(statusStr, ' ');
+                
                 [~, x, y] = Eyelink('targetcheck');
                 set(obj.dot, 'XData', x, 'YData', y)
-                obj.stim.position = [x,y];
-                obj.stim.visible = true;
+                
+                title(obj.ax, statusStr)
+                if ~strcmp(status{3}, '0') % visible
+                    newPosition = [x-obj.eyelinkConfig.screen_pixel_coords(3)/2, ...
+                        y-obj.eyelinkConfig.screen_pixel_coords(4)/2];
+                    groupStimuli('start')
+                    set(obj.stim, 'position', newPosition)
+                    set(obj.stim, 'visible', true);
+                    groupStimuli('end')
+                else
+                    set(obj.stim, 'visible', false);
+                end
                 pause(0.5);
+%                 result = Eyelink('CalResult')
+            end
+            delete(obj)
+        end
+        
+        function obj = onKeyPress(obj, ~, callbackdata)            
+            key = callbackdata.Key;
+            
+            if Eyelink('IsConnected') == 1                
+                [isMember, index] = ismember(key, obj.specialKeysNames);
+                isSingleLetter = isequal(regexp(key, '\w'),1);
+                if isMember
+                    Eyelink('SendKeyButton', obj.specialKeysValues{index}, 0, obj.KB_PRESS );
+                elseif isSingleLetter
+                    Eyelink('SendKeyButton', double(key), 0, obj.KB_PRESS );
+                else
+                    fprintf('Keypress %s ignored\n', key)
+                end
             end
         end
         
-        function obj = onKeyPress(obj, ~, callbackdata)
-            disp(callbackdata.Key)
-            KB_PRESS   = 10; % Eyelink.h
-            if ~(Eyelink('IsConnected') == -1)
-                Eyelink('SendKeyButton', double(callbackdata.Key), 0, KB_PRESS );
-            end
+        function onClose(obj, varargin)
+            obj.stopEvent.trigger();
+            delete(obj.fig);
+            
+        end
+        
+        
+        function delete(obj)
+            %             close('all')
+            delete(obj.stim)
+            
+            StimServer.Disconnect();
+            delete(obj.stimServerProcess);
+            delete(obj.stopEvent);
+            %             delete(obj.fig);
         end
     end
+    
 end
 
 % %cfg.send_config_to_tracker();
--- arcade/External/EyelinkToolbox/EyelinkConfig.m
+++ arcade/External/EyelinkToolbox/EyelinkConfig.m
@@ -2,54 +2,50 @@ classdef EyelinkConfig < handle
     
     properties ( Access = public )
         
-        sample_rate = 500;
+        
         
         % calibration
-        calibration_type = 'HV9';
+        calibration_type = 'HV5';
         
-        generate_default_targets = 'NO';
+        
         enable_automatic_calibration = 'YES';
-        randomize_calibration_order = 'YES';
-        autocal_minimum_fixation = 500; % ms
-        autocal_minimum_saccade = 2.5; % degree
+        randomize_calibration_order = 'NO';
+       
         automatic_calibration_pacing = 1000; % ms
         randomize_validation_order = 'YES';
         cal_repeat_first_target = 'YES';
         val_repeat_first_target = 'YES';
-        calibration_targets = [320,240, 320,40  320,440  40,240  600,240  40,40  600,40, 40,440  600,440];
-        calibration_samples = 20;
-        calibration_average = 'YES';
-        calibration_sequence = [0,1,2,3,4,5,6,7,8,0]
         
-        elcl_select_configuration = 'BLRR';
         
-        % drift correction
-        drift_correction_fraction = 1.0;
-        online_dcorr_button = 'ON';
-        normal_click_dcorr = 'ON';
+        
+        
+        elcl_select_configuration = 'MTABLER';%'BLRR';
+        
         
         % analog
         analog_dac_range = [-10 10];
         analog_out_data_type = 'GAZE';
         analog_binocular_mapping = 'NO';
         analog_no_pupil_value = 0.0;
-                
+        
         heuristic_filter = [2 0];
         aux_mouse_simulation = 'YES';
         
         % screen
-        screen_phys_coords = [-238.38, 148.05, -238.38, 148.05]; % SAMSUNG 2233RZ
+        screen_phys_coords = [-238.38, 148.05, 238.38, -148.05]; % SAMSUNG 2233RZ
         screen_pixel_coords = [0 0 1680 1050]; % SAMSUNG 2233RZ
-        screen_distance = 600;
+        
         
         file_event_data = 'GAZE,GAZERES,HREF,AREA,VELOCITY';
         link_event_data = 'GAZE,GAZERES,HREF,AREA,FIXAVG,NOSTART';
         file_sample_data = 'LEFT,RIGHT,GAZE,PUPIL,AREA,INPUT,STATUS';
         link_sample_data = 'LEFT,RIGHT,GAZE,PUPIL';
         
-        % current eye
-        binocular_enabled = 'YES';
+        % camera options
+        binocular_enabled = 'NO';
         active_eye = 'LEFT';
+        use_ellipse_fitter = 'NO';
+        
         %  <pupil L> <hcam>  <pupil R> <CR L> <CR R>
         initial_thresholds = [80, 40, 80, 150, 150];
         
@@ -58,6 +54,22 @@ classdef EyelinkConfig < handle
         saccade_acceleration_threshold = 8000;
         saccade_motion_threshold = 0.1;
         
+        % not functional
+%         sample_rate = 500;
+%         screen_distance = 600;
+%         elcl_tt_power = 2;
+%         calibration_samples = 15;
+%         calibration_average = 'YES';
+%         calibration_sequence = [0:4 0:4 0:4]
+%         manual_collection_minimum_fixation = 100;
+%         manual_collection_fixation_lookback = 50;
+%         autocal_minimum_fixation = 500; % ms
+%         autocal_minimum_saccade = 2.5; % degree
+%         generate_default_targets = 'NO';
+%         drift_correction_fraction = 1.0;
+%         online_dcorr_button = 'ON';
+%         normal_click_dcorr = 'ON';
+%         calibration_targets = [336,525, 840,525  1344,525,  840,840 840,210];
         
     end
     
@@ -120,13 +132,12 @@ classdef EyelinkConfig < handle
     methods ( Static )
         
         function param = read_param(parameterName)
-            pause(0.02)
             [~, param] = Eyelink('ReadFromTracker', parameterName);
             switch param
                 case 'Unknown Variable Name'
-                    error('Eyelink: failed to read %s from tracker, unknown variable name')
+                    error('Eyelink: failed to read %s from tracker, unknown variable name', parameterName)
                 case 'Variable read not supported'
-                    error('Eyelink: failed to read %s from tracker, variable not supported')
+                    error('Eyelink: failed to read %s from tracker, variable not supported', parameterName)
             end
             
         end
