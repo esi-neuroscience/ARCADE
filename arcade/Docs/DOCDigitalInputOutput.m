26c0fa8c7db605d1e32f5f80312eec20ceeb7183
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2017-10-19 14:38:06 +0200

    FIX: eventmarker checks NidaqServer connection status once

---
+++ arcade/Docs/DOCDigitalInputOutput.m
@@ -0,0 +1,8 @@
+%% Tracking digital inputs
+% ARCADE can track and respond to digitial inputs delivered via National
+% Instruments PCI(e) cards such as the PCI-6221 and PCI-6503 models.
+%
+% Tracking a digital input line is configured via the |trackdigitalinput| 
+% function, which sends a message to the |DaqServer|, which is currently
+% always the |NidaqServer.exe|.
+help trackdigitalinput
