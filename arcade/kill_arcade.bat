9341dfb00d49b21e97d25a39884541a2db04bed3
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2018-10-12 11:58:46 +0200

    Modifed start/kill scripts
    - PATH is not modified anymore
    - kill_arcade.bat also kills NidaqServer and EyeLinkServer

--- arcade/kill_arcade.bat
+++ arcade/kill_arcade.bat
@@ -1,3 +1,4 @@
 taskkill /f /im MATLAB* 
 taskkill /f /im StimServer.exe
-taskkill /f /im DaqServer.exe
+taskkill /f /im NidaqServer.exe
+taskkill /f /im EyeLinkServer.exe
--- arcade/start_arcade.bat
+++ arcade/start_arcade.bat
@@ -1,13 +1,13 @@
 @Echo OFF
 SETLOCAL
 :: 64-bit version of matlab 2014a
-SET PATH="C:\Program Files\MATLAB\R2014a\bin\";"C:\Program Files\MATLAB\R2014a\bin\win64";C:\Windows\system32;C:\Windows;%~dp0StimServer\;%~dp0External\EyelinkToolbox;%~dp0DaqServer\;"
+rem SET PATH="C:\Program Files\MATLAB\R2014a\bin\";"C:\Program Files\MATLAB\R2014a\bin\win64";C:\Windows\system32;C:\Windows;%~dp0StimServer\;%~dp0External\EyelinkToolbox;%~dp0DaqServer\;"
 SET ARCADEDIR="%~dp0"\ProcessLaunch\
 
 IF "%~1"=="/DEBUG" (
-	SET RUNCMD=matlab -r "dbstop if error; run('%ARCADEDIR%runCore.m')";
+	SET RUNCMD="C:\Program Files\MATLAB\R2014a\bin\matlab.exe" -r "dbstop if error; run('%ARCADEDIR%runCore.m')";
 ) ELSE (
-	SET RUNCMD=matlab -nodesktop -r "run('%ARCADEDIR%runCore.m')"
+	SET RUNCMD="C:\Program Files\MATLAB\R2014a\bin\matlab.exe" -nodesktop -r "run('%ARCADEDIR%runCore.m')"
 )
 
 %RUNCMD%
