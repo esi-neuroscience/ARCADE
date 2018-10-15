e1ce3801ed6e61e9277100e73e6a93d84261db16
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2018-10-15 14:06:31 +0200

    Added waiting for EyServer done event when stopping
    Also:
    - changed printing to log function

--- arcade/ProcessLaunch/SGLCoreProc.m
+++ arcade/ProcessLaunch/SGLCoreProc.m
@@ -14,8 +14,8 @@ classdef (Sealed) SGLCoreProc < SPCServerProc
     
     %------------------------------------------%
     %  2.5.2016 - Jarrod, wrote class
-    % 16.5.2016 - Jarrod, 
-    % - debugged class 
+    % 16.5.2016 - Jarrod,
+    % - debugged class
     
     methods (Static)
         function this = launch
@@ -56,19 +56,17 @@ classdef (Sealed) SGLCoreProc < SPCServerProc
             if ~ishghandle(MSgui.hfig)
                 delete(MSgui)
                 fclose('all');
-                return    
+                return
             end
             
-                        
+            
             cfg = MSgui.cfg; % get cfg file
             cfg.taskFile = MSgui.taskFile;
             
             % close main screen
             delete(MSgui);
             drawnow()
-            
-            
-            this.mWriteToDiary('Starting Event Server', true);
+                                    
             evtFile = fullfile(cfg.filepaths.Behaviour, [cfg.Subject '_' today() '_' cfg.Experiment '_' cfg.Session '.evt']);
             eventServer = SGLEventMarkerServer.launch(evtFile);
             
@@ -76,30 +74,32 @@ classdef (Sealed) SGLCoreProc < SPCServerProc
             SGLTrialDataPipe.Create();
             
             % launch ControlScreen process
-            this.mWriteToDiary('Starting ControlScreen', true)
+            logmessage('Starting ControlScreen')
             controlScreenReadyEvt = IPCEvent('controlScreenReady');
-            controlScreenProcess = this.mLaunchServer('ControlScreen'); 
+            controlScreenProcess = this.mLaunchServer('ControlScreen');
             
-            % launch EyeServer process            
-            if ~strcmp(cfg.EyeServer, 'None')
-                this.mWriteToDiary('Starting EyeServer', true)
-                eyeProcess = processManager('id', 'EyeServer', ...
-                    'command', fullfile(arcaderoot, 'arcade', 'EyeServer', ['EyeServer.bat ' cfg.EyeServer]), ...
-                    'printStdout', false, ...
-                    'printStderr', false);
-            end
-            % eyeProcess = this.mLaunchServer('EyeServer');
+            % launch EyeServer process
+            switch cfg.EyeServer
+                case 'BinaryEyelink'
+                    logmessage('Starting EyeServer')
+                    eyeProcess = processManager('id', 'EyeServer', ...
+                        'command', fullfile(arcaderoot, 'arcade', 'EyeServer', 'EyeLinkServer.exe'), ...
+                        'printStdout', false, ...
+                        'printStderr', false);
+                otherwise
+                    error('Unsupported EyeServer type')
+            end                                    
             
             % launch StimServer process
-            this.mWriteToDiary('Starting StimServer', true)
+            logmessage('Starting StimServer')
             stimServerProcess = processManager('id', 'StimServer', ...
                 'command', fullfile(arcaderoot, 'arcade', 'StimServer', 'StimServer.exe'), ...
                 'printStdout', false, ...
                 'printStderr', false);
-
+            
             % launch DaqServer process
             if ~strcmp(cfg.DaqServer, 'None')
-                this.mWriteToDiary('Starting DaqServer', true)                
+                logmessage('Starting DaqServer')
                 daqProcess = processManager('id', 'DaqServer', ...
                     'command', fullfile(arcaderoot, 'arcade', 'DaqServer', 'NidaqServer.exe'), ...
                     'printStdout', false, ...
@@ -108,63 +108,69 @@ classdef (Sealed) SGLCoreProc < SPCServerProc
             
             
             % Wait for EyeServer and ControlScreen
-            this.mWriteToDiary('Waiting for processes', true)
-%             if ~strcmp(cfg.EyeServer, 'None')
-%                 result = eyeServerReadyEvt.waitForTrigger(20000);
-%                 assert(result==1, 'Wait for EyeServer failed')
-%             end
+            logmessage('Waiting for processes')
+            %             if ~strcmp(cfg.EyeServer, 'None')
+            %                 result = eyeServerReadyEvt.waitForTrigger(20000);
+            %                 assert(result==1, 'Wait for EyeServer failed')
+            %             end
             result = controlScreenReadyEvt.waitForTrigger(20000);
             assert(result==1, 'Wait for ControlScreen failed')
             
-                      
-                       
-            % connect to StimServer            
+            
+            
+            % connect to StimServer
             StimServer.Connect();
-
+            
             % connect to DaqServer
-             if ~strcmp(cfg.DaqServer, 'None')
+            if ~strcmp(cfg.DaqServer, 'None')
                 DaqServer.Connect();
             end
             
-
+            
             % connect to EyeServer
             if ~strcmp(cfg.EyeServer, 'None')
                 EyeServer.Connect();
-                EyeServer.Start([datestr(now,'mmddHHMM') '.edf'])               
+                EyeServer.Start([datestr(now,'mmddHHMM') '.edf'])
             end
-
+            
             % Run session
             startEvent = IPCEvent('startControlScreenLoop');
             startEvent.trigger();
-                        
-            this.mWriteToDiary('Starting Session', true);
+            
+            logmessage('Starting Session');
             this.mRunSession(cfg);
             
             
             
-
+            
             
             %----------------------------------------%
             
             % delete objects
-            this.mWriteToDiary('Closing', true);
+            logmessage('Closing');
             
             delete(eventServer);
-
+            
             % quit eye server
             if ~strcmp(cfg.EyeServer, 'None')
                 eyeFile =  fullfile(cfg.filepaths.Behaviour, ...
                     [cfg.Subject '_' today() '_' cfg.Experiment '_' cfg.Session '.edf']);
                 EyeServer.Stop(eyeFile)
-                % eyeProcess.stop()
+                waitForFileEvt = IPCEvent('EyeServerDone');
+                logmessage('Waiting for eye data transfer')
+                result = waitForFileEvt.waitForTrigger(300000);
+                if ~result
+                    warning('Could not transfer eye data')
+                end
+                eyeProcess.stop()
             end
             
             % quit control screen
-            stopControlScreenEvt = IPCEvent('StopControlScreen');            
-            stopControlScreenEvt.trigger()           
+            stopControlScreenEvt = IPCEvent('StopControlScreen');
+            stopControlScreenEvt.trigger()
             controlScreenProcess.stop()
             
-            % quit stim server            
+            % quit stim server
             StimServer.delete()
             stimServerProcess.stop()
             
@@ -173,11 +179,11 @@ classdef (Sealed) SGLCoreProc < SPCServerProc
                 DaqServer.delete();
                 daqProcess.stop();
             end
-
-
+            
+            
             % close TrialData pipe
             SGLTrialDataPipe.delete()
-
+            
             fclose('all'); % close all open files
         end
     end
@@ -187,7 +193,7 @@ classdef (Sealed) SGLCoreProc < SPCServerProc
         function process = mLaunchServer(this,xServ)
             
             % are we in debug mode?
-            debugPoints = dbstatus();            
+            debugPoints = dbstatus();
             isInDebugMode = any(arrayfun(@(x) strcmp(debugPoints.cond, 'error'), ...
                 debugPoints));
             
@@ -196,22 +202,22 @@ classdef (Sealed) SGLCoreProc < SPCServerProc
                 sprintf('run%s.m',xServ));
             
             if ~isInDebugMode
-                launchCmd = ['C:\Program Files\MATLAB\R2014a\bin\' sprintf('matlab -automation -wait -r "run(''%s'')"', ...
-                    launchFunc)];
+                launchCmd = sprintf('matlab -automation -wait -r "run(''%s'')"', ...
+                    launchFunc);
             else
-                launchCmd = ['C:\Program Files\MATLAB\R2014a\bin\' sprintf('matlab -wait -r "dbstop if error; run(''%s'')"', ...
-                    launchFunc)];
+                launchCmd = sprintf('matlab -wait -r "dbstop if error; run(''%s'')"', ...
+                    launchFunc);
             end
-                        
+            
             % launch process
             process = processManager('id', xServ, ...
                 'command', launchCmd, ...
                 'printStdout', false, ...
                 'printStderr', false);
-
+            
         end
-
-
+        
+        
         
         %# RUN user's Session
         function mRunSession(this,cfg)
@@ -225,7 +231,7 @@ classdef (Sealed) SGLCoreProc < SPCServerProc
             % Launch a Session
             SESSArc  = SGLSessionArc.launch;
             % Start Session
-            % this.mWriteToDiary('Starting Session', true)
+            logmessage('Starting Session')
             SESSArc.mStart;
             
             %----------------------------------------%
@@ -236,8 +242,8 @@ classdef (Sealed) SGLCoreProc < SPCServerProc
             
             %----------------------------------------%
             % leaving session
-            this.mWriteToDiary('Quitting session', true);
-
+            logmessage('Quitting session');
+            
             delete(BHVstore);
             delete(SESSArc);
             
