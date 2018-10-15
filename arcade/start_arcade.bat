dca303fd94145b1594a798dbc0b301496107d1d5
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2018-10-15 14:08:20 +0200

    Removed explicit path of matlab.exe

--- arcade/start_arcade.bat
+++ arcade/start_arcade.bat
@@ -5,9 +5,9 @@ rem SET PATH="C:\Program Files\MATLAB\R2014a\bin\";"C:\Program Files\MATLAB\R201
 SET ARCADEDIR="%~dp0"\ProcessLaunch\
 
 IF "%~1"=="/DEBUG" (
-	SET RUNCMD="C:\Program Files\MATLAB\R2014a\bin\matlab.exe" -r "dbstop if error; run('%ARCADEDIR%runCore.m')";
+	SET RUNCMD=matlab -r "dbstop if error; run('%ARCADEDIR%runCore.m')";
 ) ELSE (
-	SET RUNCMD="C:\Program Files\MATLAB\R2014a\bin\matlab.exe" -nodesktop -r "run('%ARCADEDIR%runCore.m')"
+	SET RUNCMD=matlab -nodesktop -r "run('%ARCADEDIR%runCore.m')"
 )
 
 %RUNCMD%
