abe0fb96daa8c069360f8d9e3528b4b86855f8e9
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2019-01-31 15:13:29 +0100

    CHG: Improved documentation of Gaussian stimulus
    Changes to be committed:
    	modified:   StimServer/classes/Stimuli/Gaussian.m

--- arcade/StimServer/classes/Stimuli/Gaussian.m
+++ arcade/StimServer/classes/Stimuli/Gaussian.m
@@ -1,16 +1,29 @@
 classdef Gaussian < PixelShader
-    % GAUSSIAN - Class for a Gaussian patch stimulus
-    % ARCADE/arcade/StimServer/classes/stimfiles/pixelShader/Gaussian.fx
-    % 
-    % Size of pixel shader is always at least 5*max(sdx, sdy).
+    % GAUSSIAN < PixelShader < Stimulus - Gaussian patch for ARCADE
     %
+    % Shader file: <a href="matlab:edit('Gaussian.fx')">SquareWaveGrating.fx</a>
+    %
+    % PROPERTIES
+    % -----------
+    %   color : 24-bit color [r g b]
+    %   angle : counter-clockwise rotation of patch (degree);  0=horizontal, 90=vertical
+    %   sdx   : standard deviation in x-direction (pixel) for angle=0
+    %   sdy   : standard deviation in y-direction (pixel) for angle=0
+    %
+    % Note: The width and height of the PixelShader always at least 5*max(sdx, sdy).
+    %
+    % For more information, see <a href="matlab:doc('arcade')">the ARCADE guide</a>.
+    %
+    % See also PixelShader, Stimulus
+
+
     properties ( Transient = true )
         color = [255 255 255]; % color as 24-bit [r g b] value
         angle = 0; % rotation in degrees, 0=horizontal, 90=vertical
         sdx = 5; % standard deviation in x-direction in pixels
         sdy = 5; % standard deviation in y-direction in pixels
     end
-        
+
     methods
         function obj = Gaussian()
             % file for this stimulus
@@ -18,14 +31,14 @@ classdef Gaussian < PixelShader
             filename = fullfile(...
                 fileparts(mfilename('fullpath')), gratfxFile);
             obj = obj@PixelShader(filename);
-            
+
             % set defaults
             obj.color = obj.color;
             obj.sdx = obj.sdx;
-            obj.sdy = obj.sdy;            
+            obj.sdy = obj.sdy;
 
         end
-        
+
         function set.color(obj, rgb)
             obj.setColor(1, [rgb 255])
             obj.color = rgb;
@@ -36,7 +49,7 @@ classdef Gaussian < PixelShader
                 obj.shaderWidth = 5*max([sdx obj.sdy]);
                 obj.shaderHeight = 5*max([sdx obj.sdy]);
             end
-            obj.setParameter(1, sdx) 
+            obj.setParameter(1, sdx)
             obj.sdx = sdx;
         end
 
@@ -45,14 +58,14 @@ classdef Gaussian < PixelShader
                 obj.shaderWidth = 5*max([sdy obj.sdx]);
                 obj.shaderHeight = 5*max([sdy obj.sdx]);
             end
-            obj.setParameter(2, sdy) 
+            obj.setParameter(2, sdy)
             obj.sdy = sdy;
         end
-        
+
         function set.angle(obj, rotation)
             obj.setParameter(3, rotation)
             obj.angle = rotation;
-        end            
-     
+        end
+
     end
 end
