5ea5f076020a3ad32c4353682b6f3b2a2d7f5516
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2019-02-11 17:18:39 +0100

    CHG: start_arcade.bat can have path to _cfg.mat file as input
     Changes to be committed:
    	modified:   arcade/start_arcade.bat

--- arcade/start_arcade.bat
+++ arcade/start_arcade.bat
@@ -4,10 +4,11 @@ SETLOCAL
 rem SET PATH="C:\Program Files\MATLAB\R2014a\bin\";"C:\Program Files\MATLAB\R2014a\bin\win64";C:\Windows\system32;C:\Windows;%~dp0StimServer\;%~dp0External\EyelinkToolbox;%~dp0DaqServer\;"
 SET ARCADEDIR="%~dp0"\Core\
 
+
 IF "%~1"=="/DEBUG" (
-	SET RUNCMD=matlab -r "dbstop if error; run('%ARCADEDIR%runCore.m')";
+	SET RUNCMD=matlab -r "dbstop if error; runCore";
 ) ELSE (
-	SET RUNCMD=matlab -nodesktop -r "run('%ARCADEDIR%runCore.m')"
+	SET RUNCMD=matlab -nodesktop -r "runCore('%~1')"
 )
 
 %RUNCMD%
