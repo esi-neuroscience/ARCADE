44d8a5884663a92b1d0f3e9a2902f666592e1668
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2022-04-07 12:19:51 +0200

    CHG: Modified ARCADE update script
    - similar to arcadeVersion.m, account for the fact that git might
      not be installed on host
    
     On branch master
     Changes to be committed:
    	modified:   arcade/update_arcade.bat

--- arcade/update_arcade.bat
+++ arcade/update_arcade.bat
@@ -1,4 +1,13 @@
-git pull --all
+@Echo OFF
 
+git pull --all 2>NULL
+
+if %ERRORLEVEL% neq 0 goto gitError
+
+@rem If git is available pull from repo and wait for button press then exit w/return 0
 pause
+exit /b 0
 
+:gitError
+@rem if git is not available point user to GH releases page
+ECHO It seems git is not installed. Please update ARCADE manually by downloading a newer version from https://github.com/esi-neuroscience/ARCADE/releases
